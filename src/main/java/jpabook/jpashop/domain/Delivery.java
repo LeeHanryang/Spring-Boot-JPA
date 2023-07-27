c267c4d7a9009e6e10123da18842d47151e1934f
이준혁 <ballack02@naver.com>
2023-07-27 15:44:12 +0900

    Section 2 @ManyToOne, @OneToOne -> (fetch = FetchType.LAZY) 추가 cascade 추가 (영속성 전이) 연관관계 메서드 추가

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -15,7 +15,7 @@ public class Delivery {
     @Column(name = "delivery_id")
     private String id;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
 
     @Embedded
